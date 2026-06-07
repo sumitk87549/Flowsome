69fcd107a0f1e8187f38195bd9a02993bde47230
Sumit Kumar <sumitk87549@gmail.com>
2026-06-07 16:04:34 +0530

    .

--- flowsome/App.tsx
+++ flowsome/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>LET'S TRY TO FUCK THINGS UP AND MAKE THE WORLD DO WORK!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000ff',
+    color: '#ffe137ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
